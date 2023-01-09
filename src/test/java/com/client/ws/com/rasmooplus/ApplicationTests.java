08f37786e9c8b4b6996f7126242b773f85d96c29
Fernando <fernandocandiotti@xbrain.com.br>
2023-01-09 09:22:00 -0300

    Inicia Prjeto

---
+++ src/test/java/com/client/ws/com/rasmooplus/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.ws.com.rasmooplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
